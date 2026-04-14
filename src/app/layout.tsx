bb6f4df7947a15095d12c58d60080c8877d111ad
Ton339 <sithachad.krachaisri@gmail.com>
2026-04-14 19:49:17 +0700

    [add] icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,19 +16,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/logo.png?v=1',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/logo.png?v=1',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
+        url: '/logo.png?v=1',
+        type: 'image/png',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/logo.png?v=1',
   },
 }
 
